53f422230227997f5ad7a13c78f57e8686e640b8
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 12:38:46 +0530

    first commit

---
+++ WebApplication3_Form_HttpPostDemo/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3_Form_HttpPostDemo.Models;
+
+namespace WebApplication3_Form_HttpPostDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Add()
+        {
+            return View();
+        }
+         
+        /*
+        public ActionResult Save(int Id, string Name, double Price)
+        {
+            return Content("Saved the Details of: " + Name);
+        }                */
+
+        [HttpPost]
+        public ActionResult Add(Product prod)
+        {
+            return Content("Saved the Details of the Product:" + prod.Name);
+        }
+    }
+}
---
+++ WebApplication3_Form_HttpPostDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication3_Form_HttpPostDemo.MvcApplication" Language="C#" %>
